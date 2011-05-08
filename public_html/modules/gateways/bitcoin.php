409dd03e67d87b97cdaba5b176a58230068c1f16
Jack Alder <jack@jalder.com>
2011-05-08 14:06:51 -0400

    Fixed FriendlyName

--- public_html/modules/gateways/bitcoin.php
+++ public_html/modules/gateways/bitcoin.php
@@ -1,60 +1,20 @@
 <?php
 
-function template_config() {
+function bitcoin_config() {
     $configarray = array(
-     "FriendlyName" => array("Type" => "System", "Value"=>"My Custom Module"),
-     "username" => array("FriendlyName" => "Login ID", "Type" => "text", "Size" => "20", ),
-     "transmethod" => array("FriendlyName" => "Transaction Method", "Type" => "dropdown", "Options" => "Option1,Value2,Method3", ),
-     "instructions" => array("FriendlyName" => "Payment Instructions", "Type" => "textarea", "Rows" => "5", "Description" => "Do this then do that etc...", ),
-     "testmode" => array("FriendlyName" => "Test Mode", "Type" => "yesno", "Description" => "Tick this to test", ),
+     "FriendlyName" => array("Type" => "System", "Value"=>"Bitcoin"),
+     "username" => array("FriendlyName" => "RPC Username", "Type" => "text", "Size" => "20", ),
+     "password" => array("FriendlyName" => "RPC Password", "Type" => "text", "Size" => "20", ),
+     "host" => array("FriendlyName" => "RPC Hostname", "Type" => "text", "Size" => "20", ),
+     "port" => array("FriendlyName" => "RPC Port", "Type" => "text", "Size" => "20", ),
+     //"transmethod" => array("FriendlyName" => "Transaction Method", "Type" => "dropdown", "Options" => "Option1,Value2,Method3", ),
+     //"instructions" => array("FriendlyName" => "Payment Instructions", "Type" => "textarea", "Rows" => "5", "Description" => "Do this then do that etc...", ),
+     //"testmode" => array("FriendlyName" => "Test Mode", "Type" => "yesno", "Description" => "Tick this to test", ),
     );
 	return $configarray;
 }
 
-function template_link($params) {
-
-	# Gateway Specific Variables
-	$gatewayusername = $params['username'];
-	$gatewaytestmode = $params['testmode'];
-
-	# Invoice Variables
-	$invoiceid = $params['invoiceid'];
-	$description = $params["description"];
-    $amount = $params['amount']; # Format: ##.##
-    $currency = $params['currency']; # Currency Code
-
-	# Client Variables
-	$firstname = $params['clientdetails']['firstname'];
-	$lastname = $params['clientdetails']['lastname'];
-	$email = $params['clientdetails']['email'];
-	$address1 = $params['clientdetails']['address1'];
-	$address2 = $params['clientdetails']['address2'];
-	$city = $params['clientdetails']['city'];
-	$state = $params['clientdetails']['state'];
-	$postcode = $params['clientdetails']['postcode'];
-	$country = $params['clientdetails']['country'];
-	$phone = $params['clientdetails']['phonenumber'];
-
-	# System Variables
-	$companyname = $params['companyname'];
-	$systemurl = $params['systemurl'];
-	$currency = $params['currency'];
-
-	# Enter your code submit to the gateway...
-
-	$code = '<form method="http://www.domain.com/submit">
-<input type="hidden" name="username" value="'.$gatewayusername.'" />
-<input type="hidden" name="testmode" value="'.$gatewaytestmode.'" />
-<input type="hidden" name="description" value="'.$description.'" />
-<input type="hidden" name="invoiceid" value="'.$invoiceid.'" />
-<input type="hidden" name="amount" value="'.$amount.'" />
-<input type="submit" value="Pay Now" />
-</form>';
-
-	return $code;
-}
-
-function template_capture($params) {
+function bitcoin_capture($params) {
 
     # Gateway Specific Variables
 	$gatewayusername = $params['username'];
@@ -100,7 +60,7 @@ function template_capture($params) {
 
 }
 
-function template_refund($params) {
+function bitcoin_refund($params) {
 
     # Gateway Specific Variables
 	$gatewayusername = $params['username'];
